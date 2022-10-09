fa3e32500dba842ff56fa74d7708560b93fb8958
E4Em <109634390+E4Em@users.noreply.github.com>
2022-10-10 05:37:01 +0800

    Create ssr.sh

---
+++ ssr.sh
@@ -0,0 +1,4 @@
+mkdir -p openwrt/package/helloworld
+for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+  svn checkout "https://github.com/coolsnowwolf/packages/trunk/net/$i" "package/helloworld/$i"; \
+done
