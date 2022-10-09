cc1300882891b84f48615e6a2e3b563a1772a2e3
E4Em <109634390+E4Em@users.noreply.github.com>
2022-10-10 05:41:19 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,3 +1,4 @@
+echo 'CONFIG_PACKAGE_luci-app-ssr-plus=y' >>.config
 mkdir -p openwrt/package/helloworld
 for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
   svn checkout "https://github.com/coolsnowwolf/packages/trunk/net/$i" "package/helloworld/$i"; \
