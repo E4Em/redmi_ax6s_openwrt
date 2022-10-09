ba99c129f24f9724c8eb6ddbb1cea4de18d5e456
E4Em <109634390+E4Em@users.noreply.github.com>
2022-10-10 06:12:28 +0800

    Delete ssr.sh

--- ssr.sh
+++
@@ -1,6 +0,0 @@
-echo 'CONFIG_PACKAGE_luci-app-ssr-plus=y' >>.config
-cd openwrt
-mkdir -p package/helloworld
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
-  svn checkout "https://github.com/coolsnowwolf/packages/trunk/net/$i" "package/helloworld/$i"; \
-done
